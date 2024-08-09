8b7e3c7ac2ad0d404e2aa47e8475df579760e8ed
Kader1680 <ouldhenniabaghdad@gmail.com>
2024-08-09 19:24:54 +0100

    add the ui for home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,15 @@ import{ React, useEffect, useState} from "react"
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios'
+import Navbar from "./components/navbar";
+import Post from "./components/post";
+import Target from "./components/target";
 
 function App() {
   
   const [datas, setdata] = useState([]);
 
-  // const getData = async () => {
-  //   await axios.get("http://127.0.0.1:8000/api/data")
-  //   .then(response => {setdata(response.data)})
-  //   .then(error => console.log(error))
-  // }
+   
   useEffect(() => {
     
     
@@ -32,15 +31,34 @@ function App() {
 
   
   return (
-    <div className="box">
-       
-      {datas.map(e => {
 
-        return (
-          <div> {e.name} </div>
-        )
-      })}
+    <div>
+         <div className=" grid grid-cols-5  ">
+         <div className=" col-span-1">
+             <Navbar />
+         </div>
+         <div className=" col-span-3">
+             <Post />
+         </div>
+         <div className=" col-span-1"> 
+            <Target />
+         </div>
+            
+           
+           
+         </div>
+          <h1 className=" mt-5 mb-5  text-red-500  text-center">DFDFDFD</h1>
+
+          <div className="box b">
+              {datas.map(e => {
+
+                return (
+                  <div> {e.name} </div>
+                )
+              })}
+          </div>
     </div>
+    
   );
 }
 
---
+++ frontend/src/components/target.jsx
@@ -0,0 +1,213 @@
+import React from 'react'
+
+function Target() {
+  return (
+     
+ 
+    <div   class="max-w-sm mx-auto  ">
+       
+        <div class="bg-white shadow-lg rounded-lg  ">
+            <ul class="divide-y divide-gray-200">
+                <li class="p-3 flex justify-between items-center user-card">
+                    <div class="flex items-center">
+                        <img class="w-10 h-10 rounded-full" src="https://unsplash.com/photos/oh0DITWoHi4/download?force=true&w=640" alt="Christy" />
+                        <span class="ml-3 font-medium">Christy</span>
+                    </div>
+                    <div>
+                        <button class="text-gray-500 hover:text-gray-700">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
+                            </svg>
+                        </button>
+                    </div>
+                </li>
+                <li class="p-3 flex justify-between items-center user-card">
+                    <div class="flex items-center">
+                        <img class="w-10 h-10 rounded-full" src="https://unsplash.com/photos/7Zb7kUyQg1E/download?force=true&w=640" alt="David" />
+                        <span class="ml-3 font-medium">David</span>
+                    </div>
+                    <div>
+                        <button class="text-gray-500 hover:text-gray-700">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
+                            </svg>
+                        </button>
+                    </div>
+                </li>
+                <li class="p-3 flex justify-between items-center user-card">
+                    <div class="flex items-center">
+                        <img class="w-10 h-10 rounded-full" src="https://unsplash.com/photos/F_-0BxGuVvo/download?force=true&w=640" alt="Nancy " />
+                        <span class="ml-3 font-medium" >Nancy</span>
+                    </div>
+                    <div>
+                        <button class="text-gray-500 hover:text-gray-700">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
+                            </svg>
+                        </button>
+                    </div>
+                </li>
+                <li class="p-3 flex justify-between items-center user-card">
+                    <div class="flex items-center">
+                        <img class="w-10 h-10 rounded-full" src="https://unsplash.com/photos/3TLl_97HNJo/download?force=true&w=640" alt="Stella" />
+                        <span class="ml-3 font-medium">Stella</span>
+                    </div>
+                    <div>
+                        <button class="text-gray-500 hover:text-gray-700">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
+                            </svg>
+                        </button>
+                    </div>
+                </li>
+                <li class="p-3 flex justify-between items-center user-card">
+                    <div class="flex items-center">
+                        <img class="w-10 h-10 rounded-full" src="https://unsplash.com/photos/1AhGNGKuhR0/download?force=true&w=640" alt="Mary" />
+                        <span class="ml-3 font-medium">Mary</span>
+                    </div>
+                    <div>
+                        <button class="text-gray-500 hover:text-gray-700">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
+                            </svg>
+                        </button>
+                    </div>
+                </li>
+                <li class="p-3 flex justify-between items-center user-card">
+                    <div class="flex items-center">
+                        <img class="w-10 h-10 rounded-full" src="https://unsplash.com/photos/1AhGNGKuhR0/download?force=true&w=640" alt="Mary" />
+                        <span class="ml-3 font-medium">Mary</span>
+                    </div>
+                    <div>
+                        <button class="text-gray-500 hover:text-gray-700">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
+                            </svg>
+                        </button>
+                    </div>
+                </li>
+                <li class="p-3 flex justify-between items-center user-card">
+                    <div class="flex items-center">
+                        <img class="w-10 h-10 rounded-full" src="https://unsplash.com/photos/1AhGNGKuhR0/download?force=true&w=640" alt="Mary" />
+                        <span class="ml-3 font-medium">Mary</span>
+                    </div>
+                    <div>
+                        <button class="text-gray-500 hover:text-gray-700">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
+                            </svg>
+                        </button>
+                    </div>
+                </li>
+                <li class="p-3 flex justify-between items-center user-card">
+                    <div class="flex items-center">
+                        <img class="w-10 h-10 rounded-full" src="https://unsplash.com/photos/1AhGNGKuhR0/download?force=true&w=640" alt="Mary" />
+                        <span class="ml-3 font-medium">Mary</span>
+                    </div>
+                    <div>
+                        <button class="text-gray-500 hover:text-gray-700">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
+                            </svg>
+                        </button>
+                    </div>
+                </li>
+                <li class="p-3 flex justify-between items-center user-card">
+                    <div class="flex items-center">
+                        <img class="w-10 h-10 rounded-full" src="https://unsplash.com/photos/1AhGNGKuhR0/download?force=true&w=640" alt="Mary" />
+                        <span class="ml-3 font-medium">Mary</span>
+                    </div>
+                    <div>
+                        <button class="text-gray-500 hover:text-gray-700">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
+                            </svg>
+                        </button>
+                    </div>
+                </li>
+                <li class="p-3 flex justify-between items-center user-card">
+                    <div class="flex items-center">
+                        <img class="w-10 h-10 rounded-full" src="https://unsplash.com/photos/1AhGNGKuhR0/download?force=true&w=640" alt="Mary" />
+                        <span class="ml-3 font-medium">Mary</span>
+                    </div>
+                    <div>
+                        <button class="text-gray-500 hover:text-gray-700">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
+                            </svg>
+                        </button>
+                    </div>
+                </li>
+                <li class="p-3 flex justify-between items-center user-card">
+                    <div class="flex items-center">
+                        <img class="w-10 h-10 rounded-full" src="https://unsplash.com/photos/1AhGNGKuhR0/download?force=true&w=640" alt="Mary" />
+                        <span class="ml-3 font-medium">Mary</span>
+                    </div>
+                    <div>
+                        <button class="text-gray-500 hover:text-gray-700">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
+                            </svg>
+                        </button>
+                    </div>
+                </li>
+                <li class="p-3 flex justify-between items-center user-card">
+                    <div class="flex items-center">
+                        <img class="w-10 h-10 rounded-full" src="https://unsplash.com/photos/1AhGNGKuhR0/download?force=true&w=640" alt="Mary" />
+                        <span class="ml-3 font-medium">Mary</span>
+                    </div>
+                    <div>
+                        <button class="text-gray-500 hover:text-gray-700">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
+                            </svg>
+                        </button>
+                    </div>
+                </li>
+                <li class="p-3 flex justify-between items-center user-card">
+                    <div class="flex items-center">
+                        <img class="w-10 h-10 rounded-full" src="https://unsplash.com/photos/1AhGNGKuhR0/download?force=true&w=640" alt="Mary" />
+                        <span class="ml-3 font-medium">Mary</span>
+                    </div>
+                    <div>
+                        <button class="text-gray-500 hover:text-gray-700">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
+                            </svg>
+                        </button>
+                    </div>
+                </li>
+                <li class="p-3 flex justify-between items-center user-card">
+                    <div class="flex items-center">
+                        <img class="w-10 h-10 rounded-full" src="https://unsplash.com/photos/1AhGNGKuhR0/download?force=true&w=640" alt="Mary" />
+                        <span class="ml-3 font-medium">Mary</span>
+                    </div>
+                    <div>
+                        <button class="text-gray-500 hover:text-gray-700">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
+                            </svg>
+                        </button>
+                    </div>
+                </li>
+                <li class="p-3 flex justify-between items-center user-card">
+                    <div class="flex items-center">
+                        <img class="w-10 h-10 rounded-full" src="https://unsplash.com/photos/1AhGNGKuhR0/download?force=true&w=640" alt="Mary" />
+                        <span class="ml-3 font-medium">Mary</span>
+                    </div>
+                    <div>
+                        <button class="text-gray-500 hover:text-gray-700">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
+                            </svg>
+                        </button>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+ 
+  )
+}
+
+export default Target
